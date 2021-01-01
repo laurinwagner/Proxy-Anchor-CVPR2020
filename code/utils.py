a7e929a330428ed40c12ce8af54aafd27ae6ef66
Laurin Wagner <56727297+laurinwagner@users.noreply.github.com>
2021-01-01 18:42:50 +0100

    include reranking option in the evaluation
    Include reranking as proposed in https://arxiv.org/abs/1701.08398 (for person reidentification) for the DML datasets CUB_200_2011, Cars196 and Inshop Clothing. Could be done for SOP but could not test effectiveness due compute limitations yet.  Improves recall@1 by roughly 1 percent on each dataset. Could most likely be tuned to find better values for the reranking parameters k_1, k_2 and Lambda.

--- code/utils.py
+++ code/utils.py
@@ -6,6 +6,77 @@ import json
 from tqdm import tqdm
 import torch.nn.functional as F
 import math
+import time
+from sklearn.metrics.pairwise import pairwise_distances
+
+
+
+def re_rank(q_g_dist, q_q_dist, g_g_dist, k1=50, k2=20, lambda_value=0.94):
+
+
+    original_dist = np.concatenate(
+      [np.concatenate([q_q_dist, q_g_dist], axis=1),
+       np.concatenate([q_g_dist.T, g_g_dist], axis=1)],
+      axis=0)
+    original_dist = np.power(original_dist, 2).astype(np.float32)
+    original_dist = np.transpose(1. * original_dist/np.max(original_dist,axis = 0))
+    V = np.zeros_like(original_dist).astype(np.float32)
+    initial_rank = np.argsort(original_dist).astype(np.int32)
+
+    query_num = q_g_dist.shape[0]
+    gallery_num = q_g_dist.shape[0] + q_g_dist.shape[1]
+    all_num = gallery_num
+
+    for i in range(all_num):
+        # k-reciprocal neighbors
+        forward_k_neigh_index = initial_rank[i,:k1+1]
+        backward_k_neigh_index = initial_rank[forward_k_neigh_index,:k1+1]
+        fi = np.where(backward_k_neigh_index==i)[0]
+        k_reciprocal_index = forward_k_neigh_index[fi]
+        k_reciprocal_expansion_index = k_reciprocal_index
+        for j in range(len(k_reciprocal_index)):
+            candidate = k_reciprocal_index[j]
+            candidate_forward_k_neigh_index = initial_rank[candidate,:int(np.around(k1/2.))+1]
+            candidate_backward_k_neigh_index = initial_rank[candidate_forward_k_neigh_index,:int(np.around(k1/2.))+1]
+            fi_candidate = np.where(candidate_backward_k_neigh_index == candidate)[0]
+            candidate_k_reciprocal_index = candidate_forward_k_neigh_index[fi_candidate]
+            if len(np.intersect1d(candidate_k_reciprocal_index,k_reciprocal_index))> 2./3*len(candidate_k_reciprocal_index):
+                k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index,candidate_k_reciprocal_index)
+
+        k_reciprocal_expansion_index = np.unique(k_reciprocal_expansion_index)
+        weight = np.exp(-original_dist[i,k_reciprocal_expansion_index])
+        V[i,k_reciprocal_expansion_index] = 1.*weight/np.sum(weight)
+    original_dist = original_dist[:query_num,]
+    if k2 != 1:
+        V_qe = np.zeros_like(V,dtype=np.float32)
+        for i in range(all_num):
+            V_qe[i,:] = np.mean(V[initial_rank[i,:k2],:],axis=0)
+        V = V_qe
+        del V_qe
+    del initial_rank
+    invIndex = []
+    for i in range(gallery_num):
+        invIndex.append(np.where(V[:,i] != 0)[0])
+
+    jaccard_dist = np.zeros_like(original_dist,dtype = np.float32)
+
+
+    for i in range(query_num):
+        temp_min = np.zeros(shape=[1,gallery_num],dtype=np.float32)
+        indNonZero = np.where(V[i,:] != 0)[0]
+        indImages = []
+        indImages = [invIndex[ind] for ind in indNonZero]
+        for j in range(len(indNonZero)):
+            temp_min[0,indImages[j]] = temp_min[0,indImages[j]]+ np.minimum(V[i,indNonZero[j]],V[indImages[j],indNonZero[j]])
+        jaccard_dist[i] = 1-temp_min/(2.-temp_min)
+
+    final_dist = jaccard_dist*(1-lambda_value) + original_dist*lambda_value
+    del original_dist
+    del V
+    del jaccard_dist
+    final_dist = final_dist[:query_num,query_num:]
+    return final_dist
+
 
 def l2_norm(input):
     input_size = input.size()
@@ -59,24 +130,31 @@ def proxy_init_calc(model, dataloader):
     nb_classes = dataloader.dataset.nb_classes()
     X, T, *_ = predict_batchwise(model, dataloader)
 
+
     proxy_mean = torch.stack([X[T==class_idx].mean(0) for class_idx in range(nb_classes)])
 
     return proxy_mean
 
-def evaluate_cos(model, dataloader):
+def evaluate_cos(model, dataloader,rerank=True):
     nb_classes = dataloader.dataset.nb_classes()
 
     # calculate embeddings with model and get targets
     X, T = predict_batchwise(model, dataloader)
     X = l2_norm(X)
-
+    X=X.cpu()
+    if(rerank):
+         distances_q_g=pairwise_distances(X,X)
+         distances_g_g= distances_q_g
+         distances_q_q=distances_q_g
+         distances=re_rank(distances_q_g, distances_q_q, distances_g_g, k1=50, k2=20, lambda_value=.94)
+    else:
+        distances=pairwise_distances(X,X)
     # get predictions by assigning nearest 8 neighbors with cosine
     K = 32
     Y = []
     xs = []
-    
-    cos_sim = F.linear(X, X)
-    Y = T[cos_sim.topk(1 + K)[1][:,1:]]
+    similarities= torch.from_numpy(-distances)
+    Y = T[similarities.topk(1 + K)[1][:,1:]]
     Y = Y.float().cpu()
     
     recall = []
@@ -87,42 +165,44 @@ def evaluate_cos(model, dataloader):
 
     return recall
 
-def evaluate_cos_Inshop(model, query_dataloader, gallery_dataloader):
-    nb_classes = query_dataloader.dataset.nb_classes()
+def evaluate_Inshop(model, query_dataloader, gallery_dataloader,rerank=False):
+    # nb_classes = query_dataloader.dataset.nb_classes()
     
-    # calculate embeddings with model and get targets
-    query_X, query_T = predict_batchwise(model, query_dataloader)
-    gallery_X, gallery_T = predict_batchwise(model, gallery_dataloader)
+    # # calculate embeddings with model and get targets
+    query_X, query_T = predict_batchwise_inshop(model, query_dataloader)
+    gallery_X, gallery_T = predict_batchwise_inshop(model, gallery_dataloader)
     
-    query_X = l2_norm(query_X)
-    gallery_X = l2_norm(gallery_X)
-    
-    # get predictions by assigning nearest 8 neighbors with cosine
-    K = 50
-    Y = []
-    xs = []
-    
-    cos_sim = F.linear(query_X, gallery_X)
+    query_X=query_X.cpu()
+    gallery_X=gallery_X.cpu()
 
-    def recall_k(cos_sim, query_T, gallery_T, k):
-        m = len(cos_sim)
+    if(rerank):
+         distances_q_g = pairwise_distances(query_X, gallery_X)
+         distances_g_g = pairwise_distances(gallery_X, gallery_X)
+         distances_q_q = pairwise_distances(query_X, query_X)
+         distances=re_rank(distances_q_g, distances_q_q, distances_g_g, k1=6, k2=2, lambda_value=0)
+    else:
+         distances=pairwise_distances(query_X,gallery_X)
+
+    distances=torch.from_numpy(distances)
+    # calculate recall @ 1, 2, 4, 8
+    recall = []
+
+    def calc_recall_at_k_inshop(distances, query_T, gallery_T, k):
+        m = len(distances)
         match_counter = 0
 
         for i in range(m):
-            pos_sim = cos_sim[i][gallery_T == query_T[i]]
-            neg_sim = cos_sim[i][gallery_T != query_T[i]]
+            pos_dist = distances[i][gallery_T == query_T[i]]
+            neg_dist = distances[i][gallery_T != query_T[i]]
+            thresh = torch.min(pos_dist).item()
 
-            thresh = torch.max(pos_sim).item()
-
-            if torch.sum(neg_sim > thresh) < k:
+            if torch.sum(neg_dist < thresh) < k:
                 match_counter += 1
             
         return match_counter / m
-    
-    # calculate recall @ 1, 2, 4, 8
-    recall = []
+
     for k in [1, 10, 20, 30, 40, 50]:
-        r_at_k = recall_k(cos_sim, query_T, gallery_T, k)
+        r_at_k = calc_recall_at_k_inshop(distances, query_T, gallery_T, k)
         recall.append(r_at_k)
         print("R@{} : {:.3f}".format(k, 100 * r_at_k))
                 
